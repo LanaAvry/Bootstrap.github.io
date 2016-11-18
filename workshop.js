40e9ddb832617edd4c8bb591bbe5fb2dbc481049
Lana Avry <lana.avry@gmail.com>
2016-11-18 22:47:47 +0200

    Add files via upload

---
+++ workshop.js
@@ -0,0 +1,24 @@
+// $('.menu_icon, .close').on('click', function(){
+	$('.menu-mobile--itself').toggleClass('show')
+}) //
+
+$(document).ready(function() {
+  $("[data-toggle]").click(function() {
+    var toggle_el = $(this).data("toggle");
+    $(toggle_el).toggleClass("open-sidebar");
+  });
+
+});
+$(".swipe-area").swipe({
+    swipeStatus:function(event, phase, direction, distance, duration, fingers)
+        {
+            if (phase=="move" && direction =="right") {
+                 $(".container").addClass("open-sidebar");
+                 return false;
+            }
+            if (phase=="move" && direction =="left") {
+                 $(".container").removeClass("open-sidebar");
+                 return false;
+            }
+        }
+});
